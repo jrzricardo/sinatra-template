077270062b84dbb5c06163a04d3a507c6c2caa17
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-05-28 02:39:59 +0000

    styling

--- app.rb
+++ app.rb
@@ -3,10 +3,21 @@ require "sinatra/reloader"
 require "http"
 
 get("/") do
+
+  erb(:dog_house)
+end
+
+post("/fetch_dog") do
   @dog_url = HTTP.get("https://dog.ceo/api/breeds/image/random")
   
   @parsed_dog_url = JSON.parse(@dog_url)
 
   @dog_link = @parsed_dog_url.fetch("message")
-  erb(:index)
+
+  @dog_link_array = @dog_link.split("/")
+
+  @dog_name = @dog_link_array.at(4)
+  
+  @clean_dog_name = @dog_name.capitalize.gsub("-", " ")
+  erb(:fetch_dog)
 end
